c68fe79de5eac99beb5c15ee0966b5d2641cc10c
- <myflexflash@gmail.com>
2022-01-12 11:25:33 +0800

    rename and remove enmerable

--- contracts/Apes.sol
+++ contracts/Apes.sol
@@ -4,10 +4,9 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 
-contract Apes is Ownable, ERC721Enumerable, ReentrancyGuard {
+contract Apes is Ownable, ReentrancyGuard, ERC721 {
     using Strings for uint256;
 
     // variables for mint
@@ -19,12 +18,8 @@ contract Apes is Ownable, ERC721Enumerable, ReentrancyGuard {
 
     uint256[10000] private _availableTokens;
     uint256 private _numAvailableTokens = 10000;
-    uint256 private _numFreeRollsGiven = 0;
-
-    mapping(address => uint256) public freeRollApes;
-
     uint256 private _lastTokenIdMintedInInitialSet = 10000;
-
+   
     // variables for market
     struct Offer {
         bool isForSale;
@@ -41,46 +36,31 @@ contract Apes is Ownable, ERC721Enumerable, ReentrancyGuard {
         uint value;
     }
 
-    // A record of punks that are offered for sale at a specific minimum value, and perhaps to a specific person
-    mapping (uint => Offer) public punksOfferedForSale;
+    // A record of apes that are offered for sale at a specific minimum value, and perhaps to a specific person
+    mapping (uint => Offer) public apesOfferedForSale;
 
-    // A record of the highest punk bid
-    mapping (uint => Bid) public punkBids;
+    // A record of the highest ape bid
+    mapping (uint => Bid) public apesBids;
 
     mapping (address => uint) public pendingWithdrawals;
 
-    event PunkOffered(uint indexed tokenId, uint minValue, address indexed toAddress);
-    event PunkBidEntered(uint indexed tokenId, uint value, address indexed fromAddress);
-    event PunkBidWithdrawn(uint indexed tokenId, uint value, address indexed fromAddress);
-    event PunkBought(uint indexed tokenId, uint value, address indexed fromAddress, address indexed toAddress);
-    event PunkNoLongerForSale(uint indexed tokenId);
+    event ApesOffered(uint indexed tokenId, uint minValue, address indexed toAddress);
+    event ApesBidEntered(uint indexed tokenId, uint value, address indexed fromAddress);
+    event ApesBidWithdrawn(uint indexed tokenId, uint value, address indexed fromAddress);
+    event ApesBought(uint indexed tokenId, uint value, address indexed fromAddress, address indexed toAddress);
+    event ApesNoLongerForSale(uint indexed tokenId);
 
 
-    constructor() ERC721("0xApes", "0xApe") {}
+    constructor() ERC721("0xApes", "0xApes") {}
     
     function numTotalApes() public view virtual returns (uint256) {
         return 10000;
     }
-        
-    function freeRollMint() public nonReentrant() {
-        uint256 toMint = freeRollApes[msg.sender];
-        freeRollApes[msg.sender] = 0;
-        uint256 remaining = numTotalApes() - totalSupply();
-        if (toMint > remaining) {
-            toMint = remaining;
-        }
-        _mint(toMint);
-    }
-
-    function getNumFreeRollApes(address owner) public view returns (uint256) {
-        return freeRollApes[owner];
-    }
 
     function mint(uint256 _numToMint) public payable nonReentrant() {
         require(isMintOn, "Sale hasn't started.");
-        uint256 totalSupply = totalSupply();
         require(
-            totalSupply + _numToMint <= numTotalApes(),
+            _numToMint <= _numAvailableTokens,
             "There aren't this many Apes left."
         );
         uint256 costForMintingApes = getCostForMintingApes(_numToMint);
@@ -171,27 +151,10 @@ contract Apes is Ownable, ERC721Enumerable, ReentrancyGuard {
         returns (uint256)
     {
         require(
-            totalSupply() + _numToMint <= numTotalApes(),
+            _numToMint <= _numAvailableTokens,
             "There aren't this many Apes left."
         );
         return 0.05 ether * _numToMint;
-    }
-
-    function getApesBelongingToOwner(address _owner)
-        external
-        view
-        returns (uint256[] memory)
-    {
-        uint256 numApes = balanceOf(_owner);
-        if (numApes == 0) {
-            return new uint256[](0);
-        } else {
-            uint256[] memory result = new uint256[](numApes);
-            for (uint256 i = 0; i < numApes; i++) {
-                result[i] = tokenOfOwnerByIndex(_owner, i);
-            }
-            return result;
-        }
     }
 
     /*
@@ -222,9 +185,6 @@ contract Apes is Ownable, ERC721Enumerable, ReentrancyGuard {
         return string(abi.encodePacked(base, _tokenURI));
     }
 
-    // contract metadata URI for opensea
-    string public contractURI;
-
     /*
     * Owner stuff
     */
@@ -236,36 +196,6 @@ contract Apes is Ownable, ERC721Enumerable, ReentrancyGuard {
 
     function endMinting() public onlyOwner {
         isMintOn = false;
-    }
-
-    function giveFreeRoll(address receiver) public onlyOwner {
-        // max number of free mints we can give to the community for promotions/marketing
-        require(_numFreeRollsGiven < 200, "already given max number of free rolls");
-        uint256 freeRolls = freeRollApes[receiver];
-        freeRollApes[receiver] = freeRolls + 1;
-        _numFreeRollsGiven = _numFreeRollsGiven + 1;
-    }
-
-    // for handing out free rolls to v1 phunk owners
-    function seedFreeRolls(
-        address[] memory tokenOwners,
-        uint256[] memory numOfFreeRolls
-    ) public onlyOwner {
-        require(
-            !saleHasBeenStarted,
-            "cannot seed free rolls after sale has started"
-        );
-        require(
-            tokenOwners.length == numOfFreeRolls.length,
-            "tokenOwners does not match numOfFreeRolls length"
-        );
-
-        // light check to make sure the proper values are being passed
-        require(numOfFreeRolls[0] <= 3, "cannot give more than 3 free rolls");
-
-        for (uint256 i = 0; i < tokenOwners.length; i++) {
-            freeRollApes[tokenOwners[i]] = numOfFreeRolls[i];
-        }
     }
 
     // for seeding the v2 contract with v1 state
@@ -294,6 +224,7 @@ contract Apes is Ownable, ERC721Enumerable, ReentrancyGuard {
             useAvailableTokenAtIndex(token);
             _safeMint(tokenOwners[i], token + 10000);
         }
+
         _lastTokenIdMintedInInitialSet = lastTokenIdMintedInInitialSetCopy;
     }
 
@@ -302,52 +233,48 @@ contract Apes is Ownable, ERC721Enumerable, ReentrancyGuard {
         _baseTokenURI = baseURI;
     }
 
-    function setContractURI(string memory _contractURI) external onlyOwner {
-        contractURI = _contractURI;
-    }
-
     /***********************************************************************************************************************
-     *   Punk Market
+     *   Ape Market
      * 
     ***********************************************************************************************************************/
 
-    function punkNoLongerForSale(uint tokenId) public onlyPunkOwner(tokenId) {
-        punksOfferedForSale[tokenId] = Offer(false, tokenId, msg.sender, 0, address(0x0));
-        emit PunkNoLongerForSale(tokenId);
+    function apesNoLongerForSale(uint tokenId) public onlyApesOwner(tokenId) {
+        apesOfferedForSale[tokenId] = Offer(false, tokenId, msg.sender, 0, address(0x0));
+        emit ApesNoLongerForSale(tokenId);
     }
 
-    function offerPunkForSale(uint tokenId, uint minSalePriceInWei) public onlyPunkOwner(tokenId) {
-        punksOfferedForSale[tokenId] = Offer(true, tokenId, msg.sender, minSalePriceInWei, address(0x0));
-        emit PunkOffered(tokenId, minSalePriceInWei, address(0x0));
+    function offerApesForSale(uint tokenId, uint minSalePriceInWei) public onlyApesOwner(tokenId) {
+        apesOfferedForSale[tokenId] = Offer(true, tokenId, msg.sender, minSalePriceInWei, address(0x0));
+        emit ApesOffered(tokenId, minSalePriceInWei, address(0x0));
     }
 
-    function offerPunkForSaleToAddress(uint tokenId, uint minSalePriceInWei, address toAddress) public onlyPunkOwner(tokenId) {
-        punksOfferedForSale[tokenId] = Offer(true, tokenId, msg.sender, minSalePriceInWei, toAddress);
-        emit PunkOffered(tokenId, minSalePriceInWei, toAddress);
+    function offerApesForSaleToAddress(uint tokenId, uint minSalePriceInWei, address toAddress) public onlyApesOwner(tokenId) {
+        apesOfferedForSale[tokenId] = Offer(true, tokenId, msg.sender, minSalePriceInWei, toAddress);
+        emit ApesOffered(tokenId, minSalePriceInWei, toAddress);
     }
 
-    function buyPunk(uint tokenId) public payable nonReentrant() {
-        Offer memory offer = punksOfferedForSale[tokenId];
-        require (offer.isForSale, "this token is not for sale");                // punk not actually for sale
-        require (offer.onlySellTo == address(0x0) || offer.onlySellTo == msg.sender, "not available for you");  // punk not supposed to be sold to this user
+    function buyApes(uint tokenId) public payable nonReentrant() {
+        Offer memory offer = apesOfferedForSale[tokenId];
+        require (offer.isForSale, "this token is not for sale");                // ape not actually for sale
+        require (offer.onlySellTo == address(0x0) || offer.onlySellTo == msg.sender, "not available for you");  // ape not supposed to be sold to this user
         require (msg.value >= offer.minValue, "invalid eth value");      // Didn't send enough ETH
-        require (offer.seller == ownerOf(tokenId), "seller is not owner"); // Seller no longer owner of punk
+        require (offer.seller == ownerOf(tokenId), "seller is not owner"); // Seller no longer owner of ape
 
         address seller = offer.seller;
 
-        _safeTransfer(seller, msg.sender, tokenId, "buying punk");
+        _safeTransfer(seller, msg.sender, tokenId, "buying apes");
 
-        punkNoLongerForSale(tokenId);
+        apesNoLongerForSale(tokenId);
         pendingWithdrawals[seller] += msg.value;
-        emit PunkBought(tokenId, msg.value, seller, msg.sender);
+        emit ApesBought(tokenId, msg.value, seller, msg.sender);
 
         // Check for the case where there is a bid from the new owner and refund it.
         // Any other bid can stay in place.
-        Bid memory bid = punkBids[tokenId];
+        Bid memory bid = apesBids[tokenId];
         if (bid.bidder == msg.sender) {
             // Kill bid and refund value
             pendingWithdrawals[msg.sender] += bid.value;
-            punkBids[tokenId] = Bid(false, tokenId, address(0x0), 0);
+            apesBids[tokenId] = Bid(false, tokenId, address(0x0), 0);
         }
     }
 
@@ -359,46 +286,46 @@ contract Apes is Ownable, ERC721Enumerable, ReentrancyGuard {
         _safeTransferETH(msg.sender, amount);
     }
 
-    function enterBidForPunk(uint tokenId) public payable {
+    function enterBidForApes(uint tokenId) public payable {
         require(ownerOf(tokenId) != address(0x0), "not minted token");
         require(ownerOf(tokenId) != address(msg.sender), "impossible for owned token");
         require (msg.value > 0, "not enough eth");
         
-        Bid memory existing = punkBids[tokenId];
+        Bid memory existing = apesBids[tokenId];
         require(msg.value > existing.value, "low than previous");
     
         if (existing.value > 0) {
             // Refund the failing bid
             pendingWithdrawals[existing.bidder] += existing.value;
         }
-        punkBids[tokenId] = Bid(true, tokenId, msg.sender, msg.value);
-        emit PunkBidEntered(tokenId, msg.value, msg.sender);
+        apesBids[tokenId] = Bid(true, tokenId, msg.sender, msg.value);
+        emit ApesBidEntered(tokenId, msg.value, msg.sender);
     }
 
-    function acceptBidForPunk(uint tokenId, uint minPrice) public onlyPunkOwner(tokenId) {
+    function acceptBidForApes(uint tokenId, uint minPrice) public onlyApesOwner(tokenId) {
         address seller = msg.sender;
-        Bid memory bid = punkBids[tokenId];
+        Bid memory bid = apesBids[tokenId];
         require(bid.value > 0 && bid.value > minPrice, "invalid bid price");
         
         _safeTransfer(seller, bid.bidder, tokenId, "win");
         
-        punksOfferedForSale[tokenId] = Offer(false, tokenId, bid.bidder, 0, address(0x0));
+        apesOfferedForSale[tokenId] = Offer(false, tokenId, bid.bidder, 0, address(0x0));
         uint amount = bid.value;
-        punkBids[tokenId] = Bid(false, tokenId, address(0x0), 0);
+        apesBids[tokenId] = Bid(false, tokenId, address(0x0), 0);
         pendingWithdrawals[seller] += amount;
-        emit PunkBought(tokenId, bid.value, seller, bid.bidder);
+        emit ApesBought(tokenId, bid.value, seller, bid.bidder);
     }
 
-    function withdrawBidForPunk(uint tokenId) public nonReentrant() {
+    function withdrawBidForApes(uint tokenId) public nonReentrant() {
         require(ownerOf(tokenId) != address(0x0), "not minted token");
         require(ownerOf(tokenId) != address(msg.sender), "impossible for owned token");
 
-        Bid memory bid = punkBids[tokenId];
+        Bid memory bid = apesBids[tokenId];
         require (bid.bidder == msg.sender, "not bidder");
 
-        emit PunkBidWithdrawn(tokenId, bid.value, msg.sender);
+        emit ApesBidWithdrawn(tokenId, bid.value, msg.sender);
         uint amount = bid.value;
-        punkBids[tokenId] = Bid(false, tokenId, address(0x0), 0);
+        apesBids[tokenId] = Bid(false, tokenId, address(0x0), 0);
         // Refund the bid money
         require(_safeTransferETH(msg.sender, amount), "failed to refund");
     }
@@ -407,20 +334,20 @@ contract Apes is Ownable, ERC721Enumerable, ReentrancyGuard {
         address from,
         address to,
         uint256 tokenId
-    ) internal virtual override(ERC721Enumerable) {
+    ) internal virtual override(ERC721) {
         super._beforeTokenTransfer(from, to, tokenId);
 
-        if (punksOfferedForSale[tokenId].isForSale) {
-            punkNoLongerForSale(tokenId);
+        if (apesOfferedForSale[tokenId].isForSale) {
+            apesNoLongerForSale(tokenId);
         }
         
         // Check for the case where there is a bid from the new owner and refund it.
         // Any other bid can stay in place.
-        Bid memory bid = punkBids[tokenId];
+        Bid memory bid = apesBids[tokenId];
         if (bid.bidder == to) {
             // Kill bid and refund value
             pendingWithdrawals[to] += bid.value;
-            punkBids[tokenId] = Bid(false, tokenId, address(0x0), 0);
+            apesBids[tokenId] = Bid(false, tokenId, address(0x0), 0);
         }
     }
 
@@ -428,7 +355,7 @@ contract Apes is Ownable, ERC721Enumerable, ReentrancyGuard {
         public
         view
         virtual
-        override(ERC721Enumerable)
+        override(ERC721)
         returns (bool)
     {
         return super.supportsInterface(interfaceId);
@@ -441,8 +368,8 @@ contract Apes is Ownable, ERC721Enumerable, ReentrancyGuard {
 		return success;
     }
 
-    modifier onlyPunkOwner(uint256 tokenId) {
-        require(ownerOf(tokenId) == msg.sender, "only for punk owner");
+    modifier onlyApesOwner(uint256 tokenId) {
+        require(ownerOf(tokenId) == msg.sender, "only for apes owner");
         _;
     }
 }
